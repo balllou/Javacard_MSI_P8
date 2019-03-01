a89e9dea2f811983322f923782d48242541025e1
Chah21 <charlotte.halbout@gmail.com>
2019-03-01 16:25:51 +0100

    fixe bug

--- client/client_tpe.py
+++ client/client_tpe.py
@@ -115,7 +115,7 @@ def checkpin(pin):
 
 while True:
     montant=input("Bonjour veuillez rentrer le crédit à prelever\n")
-    if int(montant)>0 and int(montant)<=500: 
+    if montant>str(0) and montant<=str(500): 
         r=readers()
         if r==[]:
             if affiche==0:
